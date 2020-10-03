50c9fae3b1385f3e72d1e83d284f7f5c6310d68f
ntantwan <ntantwan@gmail.com>
2020-10-02 21:24:47 -0500

    Database store

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -5,6 +5,15 @@ import * as Yup from 'yup';
 
 const CourseEditScreen = ({navigation, route}) => {
     const course = route.params.course;
+    const [submitError, setSubmitError] = useState('');
+
+    async function handleSubmit(values) {
+        const { id, meets, title } = values;
+        const course = { id, meets, title };
+        firebase.database().ref('courses').child(id).set(course).catch(error => {
+          setSubmitError(error.message);
+        });
+      }
 
     return(
         <SafeAreaView style={styles.container}>
@@ -14,7 +23,9 @@ const CourseEditScreen = ({navigation, route}) => {
                             meets: course.meets,
                             title:course.title,
                         }}
-                        validationSchema={validationSchema}>
+                        validationSchema={validationSchema}
+                        onSubmit={values => handleSubmit(values)}
+                        >
                     <Form.Field
                         name="id"
                         leftIcon="identifier"
@@ -30,6 +41,8 @@ const CourseEditScreen = ({navigation, route}) => {
                         name="title"
                         leftIcon="format-title"
                         placeholder="Introduction to programming" />
+                    <Form.Button title={'Update'} />
+                    {<Form.ErrorMessage error={submitError} visible={true} />}
                 </Form>
             </ScrollView>
         </SafeAreaView>
