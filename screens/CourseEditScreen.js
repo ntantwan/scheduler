0ef4c4a757a1fff3539942bdc8261c8260379faf
ntantwan <ntantwan@gmail.com>
2020-10-02 22:21:10 -0500

    Revert "Database store"
    This reverts commit 50c9fae3b1385f3e72d1e83d284f7f5c6310d68f.

--- screens/CourseEditScreen.js
+++ screens/CourseEditScreen.js
@@ -6,15 +6,6 @@ import { firebase } from '../utils/firebase';
 
 const CourseEditScreen = ({navigation, route}) => {
     const course = route.params.course;
-    const [submitError, setSubmitError] = useState('');
-
-    async function handleSubmit(values) {
-        const { id, meets, title } = values;
-        const course = { id, meets, title };
-        firebase.database().ref('courses').child(id).set(course).catch(error => {
-          setSubmitError(error.message);
-        });
-      }
 
     return(
         <SafeAreaView style={styles.container}>
@@ -24,9 +15,7 @@ const CourseEditScreen = ({navigation, route}) => {
                             meets: course.meets,
                             title:course.title,
                         }}
-                        validationSchema={validationSchema}
-                        onSubmit={values => handleSubmit(values)}
-                        >
+                        validationSchema={validationSchema}>
                     <Form.Field
                         name="id"
                         leftIcon="identifier"
@@ -42,8 +31,6 @@ const CourseEditScreen = ({navigation, route}) => {
                         name="title"
                         leftIcon="format-title"
                         placeholder="Introduction to programming" />
-                    <Form.Button title={'Update'} />
-                    {<Form.ErrorMessage error={submitError} visible={true} />}
                 </Form>
             </ScrollView>
         </SafeAreaView>
